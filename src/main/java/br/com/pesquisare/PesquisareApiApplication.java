2fe522b1caec938f2bc975aa290bf174a63fdc1b
Jose Geraldo <jgeraldo.sousa@gmail.com>
2021-01-26 01:50:42 -0300

    Commit inicial

---
+++ src/main/java/br/com/pesquisare/PesquisareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pesquisare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pesquisare/PesquisareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pesquisare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
